45e5242a6789b5a3ac959b3386277b5e16d8b65a
Daniel Gafni <danielgafni16@gmail.com>
2021-02-03 23:27:43 +0300

    fixed imports order

--- repalette/datasets/__init__.py
+++ repalette/datasets/__init__.py
@@ -8,7 +8,7 @@ __all__ = [
 ]
 
 from repalette.datasets.abstract import AbstractQueryDataset, AbstractRecolorDataset
-from repalette.datasets.gan_dataset import GANDataset
-from repalette.datasets.pretrain_dataset import PreTrainDataset
+from repalette.datasets.gan_dataset import GANDataset  # noqa
+from repalette.datasets.pretrain_dataset import PreTrainDataset  # noqa
 from repalette.datasets.raw_dataset import RawDataset
 from repalette.datasets.rgb_dataset import RGBDataset
